be1c391d4a4cd25100bb7b539243b5b4af113bb5
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-08-23 16:42:04 +0530

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Manoj sample Application in cloud foundry.' });
+});
+module.exports = router;
